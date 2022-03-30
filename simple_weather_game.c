ddd1f7fc5895bacbf566d4f51f6e62a1ce439414
Mathew Raju <mraju@ucsc.edu>
2022-03-30 11:26:21 -0700

    creating a new weather game

---
+++ simple_weather_game.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (){
+
+    double rainfall[5][2] = {
+        {2010, 32.4},
+        {2011, 37.9},
+        {2012, 49.8},
+        {2013, 44.0},
+        {2014, 32.9}
+    };
+
+    double average = 0;
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 2; j++){
+            if (j == 1){
+                average += rainfall[i][j];
+            }
+        }
+    }
+    average /= 5;
+    printf("Average %.2lf", average);
+
+
+
+
+
+
+    return 0;
+}
